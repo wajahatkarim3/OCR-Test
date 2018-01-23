1954689937726516ee941b2dc4ceced0ceac1cb8
Wajahat Karim <wajahatkarim3@gmail.com>
2018-01-23 18:50:16 +0500

    Integrated camera to capture images....

--- app/src/main/java/com/wajahatkarim3/ocrtest/MainActivity.java
+++ app/src/main/java/com/wajahatkarim3/ocrtest/MainActivity.java
@@ -1,14 +1,23 @@
 package com.wajahatkarim3.ocrtest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.nguyenhoanglam.imagepicker.model.Config;
+import com.nguyenhoanglam.imagepicker.model.Image;
+import com.nguyenhoanglam.imagepicker.ui.imagepicker.ImagePicker;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,6 +25,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +33,20 @@ public class MainActivity extends AppCompatActivity {
     TessBaseAPI mTess;
     String datapath = "";
 
+    ImageView imageView;
+    TextView OCRTextView;
+    ProgressDialog progressDialog;
+    AsyncTask<Bitmap, Void, String> theTask;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        OCRTextView = (TextView) findViewById(R.id.OCRTextView);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Processing image...");
 
         // init Image
         image = BitmapFactory.decodeResource(getResources(), R.drawable.test_image);
@@ -91,9 +111,64 @@ public class MainActivity extends AppCompatActivity {
 
     public void processImage(View view){
         String OCRresult = null;
-        mTess.setImage(image);
-        OCRresult = mTess.getUTF8Text();
-        TextView OCRTextView = (TextView) findViewById(R.id.OCRTextView);
-        OCRTextView.setText(OCRresult);
+        progressDialog.show();
+        progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialogInterface) {
+                if (theTask != null)
+                    theTask.cancel(true);
+            }
+        });
+        theTask = new ImageProcessTask().execute(image);
+    }
+
+    public void pickImage(View view){
+        ImagePicker.with(this)
+                .setCameraOnly(true)
+                .setMaxSize(1)
+                .setMultipleMode(false)
+                .start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == Config.RC_PICK_IMAGES && resultCode == RESULT_OK && data != null) {
+            ArrayList<Image> images = data.getParcelableArrayListExtra(Config.EXTRA_IMAGES);
+            // do your logic here...
+
+            if (images.size() > 0)
+            {
+                String path = images.get(0).getPath();
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                image = BitmapFactory.decodeFile(path,bmOptions);
+                //bitmap = Bitmap.createScaledBitmap(bitmap,parent.getWidth(),parent.getHeight(),true);
+                imageView.setImageBitmap(image);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);  // THIS METHOD SHOULD BE HERE so that ImagePicker works with fragment
+    }
+
+    class ImageProcessTask extends AsyncTask<Bitmap, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(Bitmap... bitmaps) {
+
+            mTess.setImage(bitmaps[0]);
+            return mTess.getUTF8Text();
+        }
+
+        @Override
+        protected void onPostExecute(final String text) {
+            super.onPostExecute(text);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    progressDialog.dismiss();
+                    OCRTextView.setText(text);
+                }
+            });
+        }
     }
 }
